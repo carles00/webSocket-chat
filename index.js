bce39116b6a8b19b6ba9336529a15f728921c0ee
Carles Gallardo <45995898+carles00@users.noreply.github.com>
2023-01-22 21:14:28 +0100

    basic functionality

--- index.js
+++ index.js
@@ -1,19 +1,77 @@
 let myChat = {
+    userInput: null,
+    roomCodeInput: null,
+    connectButton: null,
+    chatName: null,
+
     textInput: null,
     sendButton: null,
     messageContainer: null,
 
-    init: function () {
-        myChat.textInput = document.getElementById("input-text");
-        myChat.sendButton = document.getElementById("send-text");
-        myChat.messageContainer = document.getElementById("message-board");
+    server: null,
+    userName: null,
+    userID: null,
 
-        myChat.sendButton.addEventListener("click", myChat.sendMessage);
-        myChat.textInput.addEventListener("keyup", (e) => {
+    init: function () {
+        this.userInput = document.getElementById("user-name-input");
+        this.roomCodeInput = document.getElementById("room-code-input");
+        this.connectButton = document.getElementById("connect-button");
+
+        this.textInput = document.getElementById("input-text");
+        this.sendButton = document.getElementById("send-text");
+        this.messageContainer = document.getElementById("message-board");
+        
+        this.connectButton.addEventListener("click",()=>{
+            if(this.userInput.value && this.roomCodeInput.value){
+                this.userName = this.userInput.value;
+                this.connectToRoom(this.roomCodeInput.value);
+            }
+        });
+
+        this.sendButton.addEventListener("click", myChat.sendMessage);
+        this.textInput.addEventListener("keyup", (e) => {
             if (e.code === "Enter") {
                 myChat.sendMessage();
             }
         });
+
+        this.server = new SillyClient();
+    },
+
+    connectToRoom: function(roomCode){
+        this.server.connect("wss://ecv-etic.upf.edu/node/9000/ws",`U161671CHAT-${roomCode}`);
+        this.server.on_ready = this.onServerReady.bind(this);
+        this.server.on_user_connected = this.onUserConnected.bind(this);
+        this.server.on_user_disconnected = this.onUserDisconnected.bind(this);
+        this.server.on_message = this.onMessage.bind(this);
+    },
+
+    onServerReady: function(id){
+        this.userID = id;
+    },
+
+    onUserConnected: function(id){
+        let msg = {
+            type: "text",
+            content: `${id} connected`,
+            username: ""
+        }
+        console.log(id)
+        myChat.appendSystemMessageToBoard(msg)
+    },
+
+    onUserDisconnected: function(id){
+        let msg = {
+            type: "text",
+            content: `${id} disconnected`,
+            username: ""
+        }
+        myChat.appendSystemMessageToBoard(msg);
+    },
+
+    onMessage: function(id, msg){
+        console.log(msg);
+        myChat.appendMessageToBoard(JSON.parse(msg));
     },
 
     sendMessage: function () {
@@ -22,25 +80,22 @@ let myChat = {
             let msg = {
                 type: "text",
                 content: message,
-                username: ""
+                username: myChat.userName
             }
             myChat.appendMessageToBoard(msg);
+            myChat.server.sendMessage(JSON.stringify(msg));
         }
-    },
-
-    recieveMessage: function(msg){
-
     },
 
     appendMessageToBoard: function(msg){
         let messageDiv = document.createElement("div");
         
         let classString = "message user-message"
-        if(msg.username){
+        if(msg.username !== myChat.userName){
             classString = "message contact-message"
             let authorDiv = document.createElement("div");
             authorDiv.className = "message-author"
-            authorDiv.innerText = username;
+            authorDiv.innerText = msg.username;
             messageDiv.appendChild(authorDiv);
         }
         messageDiv.className = classString;
@@ -48,9 +103,25 @@ let myChat = {
         textDiv.className = "message-content";
         textDiv.innerText = msg.content;
         messageDiv.appendChild(textDiv);
-        myChat.messageContainer.prepend(messageDiv);  
-        myChat.messageContainer.scrollTop = myChat.messageContainer.scrollHeight;
-        myChat.textInput.value = "";
+
+        this.messageContainer.prepend(messageDiv);  
+        this.messageContainer.scrollTop = myChat.messageContainer.scrollHeight;
+        this.textInput.value = "";
+    },
+
+    appendSystemMessageToBoard: function(msg){
+        let messageDiv = document.createElement("div");
+        
+        let classString = "message sys-message"
+        messageDiv.className = classString;
+        let textDiv = document.createElement("div");
+        textDiv.className = "sys-message-content";
+        textDiv.innerText = msg.content;
+        messageDiv.appendChild(textDiv);
+
+        this.messageContainer.prepend(messageDiv);  
+        this.messageContainer.scrollTop = myChat.messageContainer.scrollHeight;
+        this.textInput.value = "";
     }
 };
 
